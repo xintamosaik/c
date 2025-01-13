2d8e8843db2712af4318c08601d2abb3e38d75e6
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-01-14 00:05:41 +0100

    test structs

--- test.c
+++ test.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
+int files()
+{
+  FILE *testfile;
+  // Open a file in read mode
+  testfile = fopen("filename.txt", "r");
+  if (testfile == NULL)
+  {
+    printf("The file does not exist.\n");
+    return 1;
+  }
+  // Store the content of the file
+  char fileContent[100];
+  // Read the content and store it inside myString
+  fgets(fileContent, 100, testfile);
+  fclose(testfile);
+  printf("\n%s", fileContent);
+  testfile = fopen("filename.txt", "w");
+  // Write some text to the file
+  fprintf(testfile, fileContent);
+  fclose(testfile);
+  return 0;
+}
 /**
  * main
  */
-int main() {
+int main()
+{
+  struct Point
+  {
+    int x;
+    int y;
+  };
+
+  struct Point here;
+  here.x = 20;
+  here.y = 40;
+
   printf("hi\n"); // \n for newline
-                  
+
   const int number = 42;
   printf("The number is %d.\n", number);
-                  
+
   const float percent = 0.01;
   printf("I'm %.2f percent done with learning C.\n", percent);
 
@@ -21,20 +54,17 @@ int main() {
   printf("%lu\n", sizeof(percent));
   printf("%lu\n", sizeof(firstLetter));
 
+  const int errorCode = files();
+  if (errorCode != 0)
+  {
+    printf("There was an error in files.\n");
+    return errorCode;
+  }
 
-  FILE *testfile;
-  // Open a file in read mode
-  testfile = fopen("filename.txt", "r");
-  // Store the content of the file
-  char fileContent[100];
-  // Read the content and store it inside myString
-  fgets(fileContent, 100, testfile);
-  fclose(testfile);
-  printf("\n%s", fileContent);
-  testfile = fopen("filename.txt", "w");
-  // Write some text to the file
-  fprintf(testfile, fileContent);
-  fclose(testfile);
+  if (errorCode == 0)
+  {
+    printf("Files were handled well.\n");
+  }
+
   return 0;
 }
-
